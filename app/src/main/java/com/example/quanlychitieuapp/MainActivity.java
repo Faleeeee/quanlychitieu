371d11a44ffbeba9cfdd7ecb985cd0832f5269c9
Faleeeee <letranthuanphat111@gmail.com>
2024-05-27 10:31:35 +0700

    strictmode

--- app/src/main/java/com/example/quanlychitieuapp/MainActivity.java
+++ app/src/main/java/com/example/quanlychitieuapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.quanlychitieuapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void buttonInternetUsage(View view) {
+        String stringURL = "https://th.bing.com/th/id/OIP.nxCTB5Bu4bmfCG9TTf0wHaHa?rs=1&pid=ImgDetMain";
+        try {
+            InputStream inputStream = (InputStream) new URL(stringURL).getContent();
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+            imageView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            textView.setText(e.toString());
+        }
     }
 }
