8a430d143c153a8f574f97dc42f91a89188550e8
Faleeeee <letranthuanphat111@gmail.com>
2024-05-29 09:41:56 +0700

    ghep code home vs dang nhap

--- app/src/main/java/com/example/quanlychitieuapp/Fragment/caiDatFragment.java
+++ app/src/main/java/com/example/quanlychitieuapp/Fragment/caiDatFragment.java
@@ -1,5 +1,6 @@
 package com.example.quanlychitieuapp.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.quanlychitieuapp.R;
+import com.example.quanlychitieuapp.sign_up;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +63,20 @@ public class caiDatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cai_dat, container, false);
+        View view = inflater.inflate(R.layout.fragment_cai_dat, container, false);
+        // Initialize the button and set the onClick listener
+        Button registerTextView = view.findViewById(R.id.registerTextView);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Action when button is clicked
+                Intent intent = new Intent(getActivity(), com.example.quanlychitieuapp.sign_up.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/quanlychitieuapp/sign_up.java
@@ -0,0 +1,35 @@
+package com.example.quanlychitieuapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.quanlychitieuapp.Fragment.caiDatFragment;
+
+public class sign_up extends AppCompatActivity {
+    Button signInTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sign_up);
+        signInTextView = findViewById(R.id.signInTextView);
+        signInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(sign_up.this, caiDatFragment.class);
+                startActivity(myintent);
+            }
+        });
+
+    }
+}
